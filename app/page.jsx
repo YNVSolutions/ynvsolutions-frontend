b2a8fe290017d603c712117467658f48c2dd7bcd
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-15 00:19:29 +0530

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import Navigation from './components/navigation'
+
+const Page = () => {
+  return (
+    <div className='w-full h-full bg-zinc-900 text-white'>
+      <Navigation />
+    </div>
+  )
+}
+
+export default Page
