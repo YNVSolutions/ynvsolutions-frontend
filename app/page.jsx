03aebdad11c2e851b84cbb2175d74cfa9f725cd3
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-16 15:16:04 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,23 @@
 import React from 'react'
 import Navigation from './components/navigation'
 import HeroSection from './components/heroSection'
+import About from "./components/about";
+import ContactUs from "./components/contactUs";
 
 const Page = () => {
    
   return (
-    <div className='w-full h-full text-white'>
+    <div>
+    <div className='w-full h-screen text-white'>
       <Navigation />
       <HeroSection />
+    </div>
+    <div className='w-full h-screen bg-zinc-900/70 pt-1 flex items-center justify-center'>
+      <About />
+    </div>
+    <div className='w-full h-screen bg-zinc-900 pt-1 flex items-center justify-center '>
+      <ContactUs />
+    </div>
     </div>
   )
 }
