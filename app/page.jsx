8f45135335ed21ebda6bd684d07caedb792915f8
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-16 14:21:34 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,14 @@
 "use client"
 import React from 'react'
 import Navigation from './components/navigation'
+import HeroSection from './components/heroSection'
 
 const Page = () => {
+   
   return (
-    <div className='w-full h-full bg-zinc-900 text-white'>
+    <div className='w-full h-full text-white'>
       <Navigation />
+      <HeroSection />
     </div>
   )
 }
