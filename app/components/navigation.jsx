333b2d6faeb3627e0b5ebc620180de012923517f
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-17 01:32:13 +0530

    Update navigation.jsx

--- app/components/navigation.jsx
+++ app/components/navigation.jsx
@@ -4,22 +4,16 @@ import React from 'react'
 const Navigation = () => {
   return (
     <>
-      <div className='navBar w-full flex justify-between items-center fixed z-[999] backdrop-blur-xs'>
+      <div className='navBar w-full flex justify-between items-center fixed z-[999] backdrop-blur-xs p-[32px]'>
         <div className='logo'>
           <img src='favicon.ico' className='w-14 h-14 rounded-full ' />
         </div>
-        <div className='flex gap-10'>
+        <div className='flex gap-10 '>
           {["Home", "About Us", "Services", "Case Studies", "Contact Us"].map((item, index) => (
-            <a href='#' key={index} className="text-lg font-light capitalize hover:text-gray-500">{item}</a>
+            <a href='#' key={index} className="text-lg font-md capitalize hover:text-gray-500">{item}</a>
           ))}
         </div>
       </div>
-
-      <style jsx>{`
-        .navBar {
-          padding: 32px;
-        }
-      `}</style>
     </>
   )
 }
