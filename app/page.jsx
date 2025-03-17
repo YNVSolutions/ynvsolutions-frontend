ccab90c8f05c02dde3acb7a04b664225fb5ec925
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-17 20:26:45 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import Navigation from './components/navigation'
-import About from './components/about' 
+import About from './components/about'
 import ContactUs from "./components/contactUs";
 
 import ServiceSection from "./components/serviceSection";
@@ -12,28 +12,29 @@ import Footer from "./components/footer";
 
 
 const Page = () => {
-   
+
   return (
     <div>
-    <div className='w-full h-screen text-white'>
-      <Navigation />
-      <About />
-    </div>
-    <div className='w-full h-screen bg-zinc-900'>
-      <ServiceSection/>
-    </div>
-    <div className='w-full h-screen bg-zinc-900/70 pt-1 flex items-center justify-center'>
-      <Projects/>
-    </div>
-    <div className='w-full h-screen bg-zinc-900 pt-1 flex items-center justify-center '>
-      <ContactUs />
-    </div>
+      <div className='w-full h-screen text-white'>
+        <Navigation />
+        <About />
+      </div>
+      <div className='w-full h-screen bg-zinc-900'>
+        <ServiceSection />
+      </div>
+      <div className='w-full h-screen bg-zinc-900/70 pt-1 flex items-center justify-center'>
+        <Projects />
+      </div>
+      <div className='w-full h-screen bg-zinc-900 pt-1 flex items-center justify-center '>
+        <ContactUs />
+      </div>
 
-    <div className='w-full bg-zinc-900/70 pt-1  '>
-    <div>
-      <Footer />
+      <div className='w-full bg-zinc-900/70 pt-1  '>
+        <div>
+          <Footer />
 
-    </div>
+        </div>
+      </div>
     </div>
   )
 }
