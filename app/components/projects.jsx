f99471530afc0c58f023883f153e528a9cd7c5cf
Manav <manavpampher@gmail.com>
2025-03-20 11:18:02 +0000

    updated file projects.jsx

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -1,68 +1,41 @@
-import React from 'react'
+import React from 'react';
+
+const projectData = [
+  {
+    title: "Project Alpha",
+    description: "A web platform for data visualization.",
+    image: "https://t4.ftcdn.net/jpg/03/08/69/75/360_F_308697506_9dsBYHXm9FwuW0qcEqimAEXUvzTwfzwe.jpg",
+    link: "#"
+  },
+  {
+    title: "Project Beta",
+    description: "An AI-powered chatbot for customer service.",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRSN7q0U6K6ciRrhPb0k4mmKMDFiQNyjXUw&s",
+    link: "#"
+  },
+  {
+    title: "Project Gamma",
+    description: "A mobile app for remote team collaboration.",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqh-XpWAkI5y-Tp8KoDkTDlmNZ0pwQK_epGw&s",
+    link: "#"
+  }
+];
 
 const Projects = () => {
   return (
-    <div className='z-[-999] absolute  w-full h-full bg-zinc-700' id = "projects">
-      <style jsx>{`
-        .grid div {
-          grid-row: var(--r);
-          grid-column: var(--c);
-        }
-        .grid img {
-          width: 200px;
-          height: 150px;
-        }
-      `}</style>
-      <div className="grid grid-cols-8 grid-rows-4 gap-2">
-        {/* Row 1 */}
-        <div className="col-span-1 row-span-1" style={{ '--r': 1, '--c': 1 }}>
-          <img src="https://t4.ftcdn.net/jpg/03/08/69/75/360_F_308697506_9dsBYHXm9FwuW0qcEqimAEXUvzTwfzwe.jpg" alt="Image 1" />
-        </div>
-        <div className="col-span-1 row-span-1" style={{ '--r': 1, '--c': 7 }}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXRSN7q0U6K6ciRrhPb0k4mmKMDFiQNyjXUw&s" alt="Image 15" />
-        </div>
-        <div className="col-span-1 row-span-1" style={{ '--r': 1, '--c': 4 }}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqh-XpWAkI5y-Tp8KoDkTDlmNZ0pwQK_epGw&s" alt="Image 15" />
-        </div>
+    <div className='z-[-999] absolute w-full h-full bg-zinc-700 p-10' id='projects'>
+      <div className="grid grid-cols-3 gap-6">
+        {projectData.map((project, index) => (
+          <div key={index} className="bg-white p-4 rounded-lg shadow-lg text-center">
+            <img src={project.image} alt={project.title} className="w-full h-40 object-cover rounded-md mb-4" />
+            <h3 className="text-lg font-semibold">{project.title}</h3>
+            <p className="text-gray-600 text-sm">{project.description}</p>
+            <a href={project.link} className="text-blue-500 mt-2 inline-block">Learn More</a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-        {/* Row 2 */}
-        <div className="col-span-1 row-span-1" style={{ '--r': 2, '--c': 5 }}>
-          <img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/Technology_Trends.jpg" alt="Image 3" />
-        </div>
-        <div className="col-span-1 row-span-1" style={{ '--r': 2, '--c': 8 }}>
-          <img src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/201810/stockvault-person-studying-and-learning---knowledge-concept178241_0.jpeg?size=690:388" alt="Image 13" />
-        </div>
-        <div className="col-span-1 row-span-1" style={{ '--r': 2, '--c': 2 }}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_d44PBXJFU95Jxbp0xZTqkP2XJGemmgiQemhn1l_RAedJRFebCVCMZz2YoxFwaffJ-U0&usqp=CAU" alt="Image 15" />
-        </div>
-
-        {/* Row 3 */}
-        <div className="col-span-1 row-span-1" style={{ '--r': 3, '--c': 4 }}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqZx_g2K2F99ksuxib171hL0hfKx8c8OopjQ&s" alt="Image 5" />
-        </div>
-        <div className="col-span-1 row-span-1" style={{ '--r': 3, '--c': 6 }}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5q8QTOBqv5D2IiYT-H2zuXNe-ostEiw9rHw&s" alt="Image 11" />
-        </div>
-        <div className="col-span-1 row-span-1" style={{ '--r': 3, '--c': 1 }}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7LyYFYMemkRO_G4Lc9mQSygeLfwP1482KHg&s" alt="Image 15" />
-        </div>
-
-        {/* Row 4 */}
-        <div className="col-span-1 row-span-1" style={{ '--r': 4, '--c': 8 }}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5y1WbbqVDtaGo2MfjArnEAIOEnWBv4PQhtA&s" alt="Image 7" />
-        </div>
-        <div className="col-span-1 row-span-1" style={{ '--r': 4, '--c': 2 }}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKSp_DSBSIuU_dG3YhmFGC_GXbYjzAz8UkXA&s" alt="Image 9" />
-        </div>
-        <div className="col-span-1 row-span-1" style={{ '--r': 4, '--c': 5 }}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_U_Hi1vlmPAmZA7oyMAAtkRZqnxpWqyB0lQ&s" alt="Image 15" />
-        </div>
-
-      </div >
-
-    </div >
-
-  )
-}
-
-export default Projects
+export default Projects;
