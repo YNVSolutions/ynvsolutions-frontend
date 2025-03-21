8f83cd839232897916c2075fb518b2aba769cd4f
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-22 01:42:19 +0530

    Update projects.jsx

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -1,38 +1,9 @@
-import Image from 'next/image'
 import React from 'react'
 
-const Projects = () => {
+const projects = () => {
   return (
-    <>
-    <div className='flex justify-center items-center pt-24 pb-20'>
-      <h1 className='text-8xl text-white font-semibold'>Projects<span className='text text-7xl'> We Done</span></h1>
-    </div>
-    <div className='flex flex-col gap-3 p-3'>
-    <div className='flex gap-3'>
-      <div className='border h-[33vh] w-[33vw]'>
-        <Image src='/img1.jpg' height={64} width={64} className='h-[32vh] w-[33vw]'/>
-      </div>
-      <div className='border h-[33vh] w-[33vw]'>
-        <Image src='/img2.jpg' height={64} width={64} className='h-[32vh] w-[33vw]'/>
-      </div>
-      <div className='border h-[33vh] w-[33vw]'>
-        <Image src='/img3.jpg' height={64} width={64} className='h-[32vh] w-[33vw]'/>
-      </div>
-    </div>
-    <div className='flex gap-3'>
-      <div className='border h-[33vh] w-[33vw]'>
-        <Image src='/img4.jpg' height={64} width={64} className='h-[32vh] w-[33vw]'/>
-      </div>
-      <div className='border h-[33vh] w-[33vw]'>
-        <Image src='/img5.jpg' height={64} width={64} className='h-[32vh] w-[33vw]'/>
-      </div>
-      <div className='border h-[33vh] w-[33vw]'>
-        <Image src='/img6.jpg' height={64} width={64} className='h-[32vh] w-[33vw]'/>
-      </div>
-    </div>
-    </div>
-    </>
+    <div>projects</div>
   )
 }
 
-export default Projects
+export default projects
