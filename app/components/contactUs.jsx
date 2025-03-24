44ac28635fb956d84f1b8e5c226fc81148902a9d
Jitesh Singh <jiteshSingh4305@gmail.com>
2025-03-24 21:31:38 +0530

    styled the input and added submit button

--- app/components/contactUs.jsx
+++ app/components/contactUs.jsx
@@ -21,37 +21,45 @@ const ContactUs = () => {
   return (
     <>
       <div
-        className=" m-4 md:m-6 mt-20 md:mt-16 w-full h-[90vh] md:min-h-[80vh] rounded-2xl bounding-box bg-gray-900 flex flex-col md:flex-row items-center justify-around shadow-2xl p-4"
+        className=" m-4 md:m-6 mt-20 md:mt-16 w-full h-[90vh] md:min-h-[80vh] rounded-2xl bounding-box bg-gray-900 
+        flex flex-col md:flex-row items-center justify-around shadow-2xl p-4"
         id="contactUs"
       >
         <div className="bg-transparent w-full h-full flex-[1.5] p-10">
-          <div className="w-full h-4/6 p-4">
-            <p className="text-white text-8xl">Contact Us</p>
+          <div className="w-full h-3/6 p-4">
+            <p className="text-white text-8xl mb-4">Contact Us</p>
+            <p className="text-gray-300 text-xl">Email us at: &nbsp;<a className="text-gray-50" href="mailto:ynvtech@gmail.com">ynvtech@gmail.com</a> &nbsp;&nbsp;OR,</p>
+            <p className="text-gray-300 text-xl">fill the form and we will reach out to you.</p>
           </div>
           <div className="flex justify-around h-2/6">
-            <div className="bg-gray-400 w-full h-full flex-1 p-8 m-2 rounded-2xl"></div>
-            <div className="bg-gray-400 w-full h-full flex-1 p-8 m-2 rounded-2xl"></div>
-            <div className="bg-gray-400 w-full h-full flex-1 p-8 m-2 rounded-2xl"></div>
+            <div className="bg-gray-600 w-full h-full flex-1 p-8 m-2 rounded-2xl">Need an app for your website?</div>
+            <div className="bg-gray-600 w-full h-full flex-1 p-8 m-2 rounded-2xl">Are you lagging in the AI race?</div>
+            <div className="bg-gray-600 w-full h-full flex-1 p-8 m-2 rounded-2xl">Custom Android kiosks that actually survive 24/7 use?</div>
           </div>
         </div>
 
         <div className="bg-transparent w-full h-full flex-1 p-8 flex items-center justify-center">
-          <div className="bg-gray-600 w-4/5 h-6/7 p-8 rounded-2xl shadow-2xl">
-          <p className="text-white text-5xl">Get in touch</p>
-          <p className="text-gray-100 text-md mb-10">Let’s discuss how we can help</p>
+          <div className="bg-gray-600 w-4/5 h-6/7 p-8 rounded-2xl shadow-2xl shadow-blue-900/30">
+            <p className="text-white text-5xl">Get in touch</p>
+            <p className="text-gray-100 text-md mb-6 ms-1">
+              Let’s discuss how we can help
+            </p>
             <form action="">
               <div className="flex w-full">
                 <input
                   placeholder="First Name"
-                  className="w-full p-3 m-1 bg-gray-800 border-none text-white border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  className="w-full p-3 m-1 bg-gray-800 border-none text-white border border-gray-600 rounded-lg 
+                  focus:outline-none focus:ring-2 focus:ring-blue-400"
                   type="text"
                   name=""
                   id=""
+                  required
                 />
                 <input
                   type="text"
                   placeholder="Last Name"
-                  className="w-full m-1 p-3 border-none bg-gray-800 text-white border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  className="w-full m-1 p-3 border-none bg-gray-800 text-white border border-gray-600 rounded-lg 
+                  focus:outline-none focus:ring-2 focus:ring-blue-400"
                 />
               </div>
               <div className="w-full flex justify-center">
@@ -59,16 +67,34 @@ const ContactUs = () => {
                   type="email"
                   name=""
                   id=""
+                  required
                   placeholder="Email address"
-                  className="w-full m-1 p-3 border-none bg-gray-800 text-white border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  className="w-full m-1 p-3 border-none bg-gray-800 text-white border border-gray-600 rounded-lg 
+                  focus:outline-none focus:ring-2 focus:ring-blue-400"
                 />
               </div>
               <div className="w-full mt-2 flex justify-center">
                 <textarea
                   placeholder="How can we help you?"
-                  rows={5} 
-                  className="w-full m-1 p-3 bg-gray-800 border-none text-white border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 resize-none"
+                  rows={5}
+                  className="w-full m-1 p-3 bg-gray-800 border-none text-white border border-gray-600 rounded-lg 
+                  focus:outline-none focus:ring-2 focus:ring-blue-400 resize-none"
                 ></textarea>
+              </div>
+              <div className="w-full flex justify-center mt-2">
+                <button
+                  onClick={notify}
+                  className="w-full m-1 p-[2px] rounded-lg group relative bg-gradient-to-r from-pink-500 via-blue-500
+                   to-pink-500  bg-[length:200%_auto] hover:bg-[position:100%_center] transition-all duration-500 ease-out 
+                   overflow-hidden">
+                  <div
+                    className="w-full h-full p-3 bg-gray-900 rounded-[calc(0.5rem-2px)]
+                 text-white text-center font-medium hover:text-gray-50
+                 group-hover:bg-gray-950 transition-colors duration-300 "
+                  >
+                    Send Message
+                  </div>
+                </button>
               </div>
             </form>
           </div>
