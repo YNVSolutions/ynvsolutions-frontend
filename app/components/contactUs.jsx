47fbafad52311b84315f723049160461bc866385
Jitesh Singh <jiteshSingh4305@gmail.com>
2025-03-24 15:30:02 +0530

    added bounding boxes

--- app/components/contactUs.jsx
+++ app/components/contactUs.jsx
@@ -19,19 +19,14 @@ const ContactUs = () => {
 
     return (
         <>
-            <div className='w-full min-h-screen flex items-center justify-center mb-32' id="contactUs"> 
-                <div className='w-[60%] border-t-pink-500 border-b-blue-500 border-l-blue-500 border-r-pink-500 border-2  p-4 sm:p-8 md:p-12 lg:p-36 flex flex-col items-center justify-center'>  
-                    <h1 className='text-white text-5xl mb-8 text'>Contact Us</h1>
-                    <div className='w-full max-w-md flex flex-col gap-4'> 
-                        <input type='text' placeholder='Name' className='p-3 text-white bg-zinc-900 border-b-2 border-pink-500 w-full text-xl' /> 
-                        <input type='email' placeholder='Email' className='p-3 text-white bg-zinc-900 border-b-2 border-blue-500 w-full text-xl' /> 
-                        <textarea placeholder='Message'  className='p-3 text-white bg-zinc-900 border-b-2 border-pink-500 w-full text-xl'></textarea> 
-                        <Link href='#'>
-                            <button className='text-white bg-green-600 p-3 w-full my-5 hover:bg-green-700 transition duration-600' onClick={notify}>Message Us</button> 
-                        </Link>
-                    </div>
-                </div>
-                <ToastContainer />
+            <div className=' m-4 md:m-6 mt-20 md:mt-16 w-full h-[90vh] md:min-h-[80vh] rounded-2xl bounding-box bg-gray-900 flex flex-col md:flex-row items-center justify-around shadow-2xl p-4' id="contactUs"> 
+            <div className='bg-red-300 w-full h-full'>
+                jhkjhb
+            </div>
+
+            <div className='bg-green-300 w-full h-full'>
+                jlojhh
+            </div>
             </div>
         </>
     )
