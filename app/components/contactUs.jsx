91bb43ae92ab8a55033a99806e4b07c140ad966a
Jitesh Singh <jiteshSingh4305@gmail.com>
2025-03-24 15:53:12 +0530

    created inner containers

--- app/components/contactUs.jsx
+++ app/components/contactUs.jsx
@@ -20,12 +20,27 @@ const ContactUs = () => {
     return (
         <>
             <div className=' m-4 md:m-6 mt-20 md:mt-16 w-full h-[90vh] md:min-h-[80vh] rounded-2xl bounding-box bg-gray-900 flex flex-col md:flex-row items-center justify-around shadow-2xl p-4' id="contactUs"> 
-            <div className='bg-red-300 w-full h-full'>
-                jhkjhb
+            <div className='bg-red-300 w-full h-full flex-[1.5] p-8'>
+                <div className='bg-red-500 w-full h-4/6 p-4'>
+                   <p className='text-black text-8xl'>Contact Us</p>
+                </div>
+                <div className='flex justify-around h-2/6'>
+                    <div className='bg-green-600 w-full h-full flex-1 p-8 m-2'>
+
+                    </div>
+                    <div className='bg-green-600 w-full h-full flex-1 p-8 m-2'>
+
+                    </div>
+                    <div className='bg-green-600 w-full h-full flex-1 p-8 m-2'>
+
+                    </div>
+                </div>
             </div>
 
-            <div className='bg-green-300 w-full h-full'>
-                jlojhh
+            <div className='bg-green-300 w-full h-full flex-1 p-8 flex items-center justify-center'>
+                <div className='bg-green-100 w-4/5 h-6/7 p-8 rounded-2xl shadow-2xl'>
+
+                </div>
             </div>
             </div>
         </>
