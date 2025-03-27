247fd710fe829ccfa99635da0a36cbf505e0c15f
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-27 19:30:17 +0530

    Update clients.jsx

--- app/components/clients.jsx
+++ app/components/clients.jsx
@@ -5,40 +5,48 @@ import React from 'react'
 const Clients = () => {
   return (
     <>
-      <div className='flex justify-center items-center w-full gap-5'>
-        <div className='text-white'>
-          <h1 className='text-3xl text pb-4'>OUR AWESOME CLIENT</h1>
-          <h1 className='text-6xl font-semibold'>We Built Best<br /> Solution For Your<br /> Company</h1>
+      <div className='flex flex-col lg:flex-row justify-center items-center w-full gap-5 p-4'>
+        <div className='text-white text-center lg:text-left'>
+          <h1 className='text-2xl lg:text-3xl pb-4 text'>OUR AWESOME CLIENT</h1>
+          <h1 className='text-4xl lg:text-6xl font-semibold'>
+            We Built Best<br /> Solution For Your<br /> Company
+          </h1>
         </div>
-        <div className=' flex flex-col justify-center items-center gap-4'>
-          <div className='flex justify-center items-center gap-4'>
+        <div className='flex flex-col justify-center items-center gap-4'>
+          <div className='flex flex-wrap justify-center items-center gap-4'>
             <div>
-              <Image
-                src='/ARX_vision.png'
-                height={64}
-                width={64}
-                alt='ARX VISION'
-                className=' border-2 border-white p-2 h-[20vh] w-[20vw] hover:w-[22vw] hover:h-[22vh] transition-all duration-300'
-              />
+              <Link href='https://arx.vision/'>
+                <Image
+                  src='/ARX_vision.png'
+                  height={64}
+                  width={64}
+                  alt='ARX VISION'
+                  className='border-2 border-white p-2 h-[15vh] w-[40vw] sm:h-[20vh] sm:w-[30vw] lg:h-[20vh] lg:w-[20vw] hover:w-[22vw] hover:h-[22vh] transition-all duration-300'
+                />
+              </Link>
             </div>
             <div>
-              <Image
-                src='/Dona.png'
-                height={64}
-                width={64}
-                alt='DONA'
-                className=' border-2 border-white p-2 h-[20vh] w-[20vw] hover:w-[22vw] hover:h-[22vh] transition-all duration-300'
-              />
+              <Link href='https://donadonations.com/'>
+                <Image
+                  src='/Dona.png'
+                  height={64}
+                  width={64}
+                  alt='DONA'
+                  className='border-2 border-white p-2 h-[15vh] w-[40vw] sm:h-[20vh] sm:w-[30vw] lg:h-[20vh] lg:w-[20vw] hover:w-[22vw] hover:h-[22vh] transition-all duration-300'
+                />
+              </Link>
             </div>
           </div>
           <div>
-            <Image
-              src='/Our_media.png'
-              height={64}
-              width={64}
-              alt='OUR MEDIA'
-              className=' border-2 border-white p-2 h-[20vh] w-[20vw] hover:w-[22vw] hover:h-[22vh] transition-all duration-300'
-            />
+            <Link href='https://ourmedia.co.uk/'>
+              <Image
+                src='/Our_media.png'
+                height={64}
+                width={64}
+                alt='OUR MEDIA'
+                className='border-2 border-white p-2 h-[15vh] w-[40vw] sm:h-[20vh] sm:w-[30vw] lg:h-[20vh] lg:w-[20vw] hover:w-[22vw] hover:h-[22vh] transition-all duration-300'
+              />
+            </Link>
           </div>
         </div>
       </div>
