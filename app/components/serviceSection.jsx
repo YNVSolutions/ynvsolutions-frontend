e1e1c42fb6b555b89c60bbd11b4dcf156aa11c1b
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-27 10:26:10 +0530

    Update serviceSection.jsx

--- app/components/serviceSection.jsx
+++ app/components/serviceSection.jsx
@@ -18,10 +18,10 @@ const ServiceSection = () => {
             <div className='flex justify-center items-center text-md'>
               <ul className='p-3'>
                 <li className='text-lg font-semibold pb-5'>What We Offer : </li>
-                <li>Cross-Platform Excellence</li>
-                <li>End-to-End Development</li>
-                <li>Utilizing Modern Tech</li>
-                <li>Reliable Platform Support</li>
+                <li>1. Cross-Platform Excellence</li>
+                <li>2. End-to-End Development</li>
+                <li>3. Utilizing Modern Tech</li>
+                <li>4. Reliable Platform Support</li>
               </ul>
               <img src='https://staging.spwebconnect.com/img/services/mobile_application/mobile_app_design/SpWebconnect_mobile_app_development.gif' className='overflow-hidden h-[15vh] md:h-[20vh] lg:h-[25vh] w-[80%] md:w-[60%] lg:w-[25vw] m-auto rounded-2xl' />
             </div>
@@ -35,10 +35,10 @@ const ServiceSection = () => {
             <div className='flex justify-center items-center'>
               <ul className='text-md p-3'>
                 <li className='text-lg font-semibold pb-5'>What We Offer :</li>
-                <li>Device Tracking & Monitoring</li>
-                <li>Secure Data Management</li>
-                <li>User & Device Identity Verification</li>
-                <li>BYOD Authentication</li>
+                <li>1. Device Tracking & Monitoring</li>
+                <li>2. Secure Data Management</li>
+                <li>3. User & Device Identity Verification</li>
+                <li>4. BYOD Authentication</li>
               </ul>
               <img src='https://www.intellium.ai/expertise_hero.svg' className='h-[15vh] md:h-[20vh] lg:h-[25vh] w-[80%] md:w-[60%] lg:w-[25vw] m-auto rounded-2xl' />
             </div>
@@ -48,13 +48,13 @@ const ServiceSection = () => {
           <div className='h-[40vh] md:h-[50vh] lg:h-[57vh] w-full bg-zinc-900 rounded-lg animate-float border-t-4 border-blue-500 cursor-pointer'>
             <h1 className='text-center p-4 text text-2xl md:text-3xl lg:text-4xl font-semibold'>AI Solution</h1>
             <p className='text-center text-sm md:text-lg lg:text-xl pt-2 pl-2 pr-2'>Our AI solution enhances website/app performance through automated decision-making and machine learning. We analyze data to provide effective insights, mimicking human intelligence for chatbots, recommendations, and fraud detection.</p>
-            <div>
-              <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+            <div className='flex justify-center items-center'>
+              <ul className='p-3'>
+                <li className='text-lg font-semibold pb-5'>What We Offer :</li>
+                <li>1. Automated Decisioning</li>
+                <li>2. Machine Learning Core</li>
+                <li>3. Data-Driven Learning</li>
+                <li>4. Real-time Data Analysis</li>
               </ul>
               <img src='https://whatmaction.com/images/services-images/ai-solutions-banner.svg' className='h-[15vh] md:h-[20vh] lg:h-[25vh] w-[80%] md:w-[60%] lg:w-[25vw] m-auto rounded-2xl' />
             </div>
@@ -62,9 +62,18 @@ const ServiceSection = () => {
 
 
           <div className='h-[40vh] md:h-[50vh] lg:h-[57vh] w-full bg-zinc-900 rounded-lg animate-float border-t-4 border-pink-500 cursor-pointer'>
-            <h1 className='text-center p-4 text text-2xl md:text-3xl lg:text-4xl font-semibold'>Web Dev</h1>
-            <p className='text-center text-sm md:text-lg lg:text-xl pt-2 pl-2 pr-2'>Responsive websites that help your business grow and encourage engagement</p>
-            <img src='https://codarity.com/wp-content/uploads/2020/05/Web-Development-Illustration.svg' className='h-[15vh] md:h-[20vh] lg:h-[25vh] w-[80%] md:w-[60%] lg:w-[25vw] m-auto rounded-2xl' />
+            <h1 className='text-center p-4 text text-2xl md:text-3xl lg:text-4xl font-semibold'>Web Development</h1>
+            <p className='text-center text-sm md:text-lg lg:text-xl pt-2 pl-2 pr-2'>We build modern and responsive websites tailored to your needs. Our web development encompasses design, functionality, and optimization for a seamless user experience. We focus on performance and utilize the latest web technologies.</p>
+            <div className='flex justify-center items-center'>
+              <ul className='p-3'>
+                <li className='text-lg font-semibold pb-5'>What We Offer :</li>
+                <li>1. User-Friendly Interfaces</li>
+                <li>2. Robust Website Features</li>
+                <li>3. Fast & Reliable Sites</li>
+                <li>4. Performance Optimization</li>
+              </ul>
+              <img src='https://codarity.com/wp-content/uploads/2020/05/Web-Development-Illustration.svg' className='h-[15vh] md:h-[20vh] lg:h-[25vh] w-[80%] md:w-[60%] lg:w-[25vw] m-auto rounded-2xl' />
+            </div>
           </div>
         </div>
       </div>
