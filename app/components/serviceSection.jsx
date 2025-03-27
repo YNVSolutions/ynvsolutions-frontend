d3295c3f26bcf86e04f247b030f957f55235202f
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-27 10:19:42 +0530

    Update serviceSection.jsx

--- app/components/serviceSection.jsx
+++ app/components/serviceSection.jsx
@@ -17,10 +17,11 @@ const ServiceSection = () => {
             </p>
             <div className='flex justify-center items-center text-md'>
               <ul className='p-3'>
-                <li>What We Offer : </li>
+                <li className='text-lg font-semibold pb-5'>What We Offer : </li>
                 <li>Cross-Platform Excellence</li>
                 <li>End-to-End Development</li>
                 <li>Utilizing Modern Tech</li>
+                <li>Reliable Platform Support</li>
               </ul>
               <img src='https://staging.spwebconnect.com/img/services/mobile_application/mobile_app_design/SpWebconnect_mobile_app_development.gif' className='overflow-hidden h-[15vh] md:h-[20vh] lg:h-[25vh] w-[80%] md:w-[60%] lg:w-[25vw] m-auto rounded-2xl' />
             </div>
@@ -30,15 +31,33 @@ const ServiceSection = () => {
 
           <div className='h-[40vh] md:h-[50vh] lg:h-[57vh] w-full bg-zinc-900 rounded-lg animate-float border-t-4 border-blue-500 cursor-pointer'>
             <h1 className='text-center p-4 text text-2xl md:text-3xl lg:text-4xl font-semibold'>MDM Solution</h1>
-            <p className='text-center text-sm md:text-lg lg:text-xl pt-2 pl-2 pr-2'>Optimized Mobile Databases: Secure, efficient data for your apps</p>
-            <img src='https://www.intellium.ai/expertise_hero.svg' className='h-[15vh] md:h-[20vh] lg:h-[25vh] w-[80%] md:w-[60%] lg:w-[25vw] m-auto rounded-2xl' />
+            <p className='text-center text-sm md:text-lg lg:text-xl pt-2 pl-2 pr-2'>Our MDM solution ensures complete privacy and security for your company devices. We offer device tracing, mobile management with data security, identity and access management, and BYOD authentication. Benefit from time savings, improved efficiency, and enhanced security.</p>
+            <div className='flex justify-center items-center'>
+              <ul className='text-md p-3'>
+                <li className='text-lg font-semibold pb-5'>What We Offer :</li>
+                <li>Device Tracking & Monitoring</li>
+                <li>Secure Data Management</li>
+                <li>User & Device Identity Verification</li>
+                <li>BYOD Authentication</li>
+              </ul>
+              <img src='https://www.intellium.ai/expertise_hero.svg' className='h-[15vh] md:h-[20vh] lg:h-[25vh] w-[80%] md:w-[60%] lg:w-[25vw] m-auto rounded-2xl' />
+            </div>
           </div>
 
 
           <div className='h-[40vh] md:h-[50vh] lg:h-[57vh] w-full bg-zinc-900 rounded-lg animate-float border-t-4 border-blue-500 cursor-pointer'>
             <h1 className='text-center p-4 text text-2xl md:text-3xl lg:text-4xl font-semibold'>AI Solution</h1>
-            <p className='text-center text-sm md:text-lg lg:text-xl pt-2 pl-2 pr-2'>Leverage AI to automate processes, enhance efficiency, and make smarter decisions</p>
-            <img src='https://whatmaction.com/images/services-images/ai-solutions-banner.svg' className='h-[15vh] md:h-[20vh] lg:h-[25vh] w-[80%] md:w-[60%] lg:w-[25vw] m-auto rounded-2xl' />
+            <p className='text-center text-sm md:text-lg lg:text-xl pt-2 pl-2 pr-2'>Our AI solution enhances website/app performance through automated decision-making and machine learning. We analyze data to provide effective insights, mimicking human intelligence for chatbots, recommendations, and fraud detection.</p>
+            <div>
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+              <img src='https://whatmaction.com/images/services-images/ai-solutions-banner.svg' className='h-[15vh] md:h-[20vh] lg:h-[25vh] w-[80%] md:w-[60%] lg:w-[25vw] m-auto rounded-2xl' />
+            </div>
           </div>
 
 
