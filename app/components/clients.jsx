b5f56356016472b11aff5c46573d8df7ab24890c
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-27 19:04:35 +0530

    Update clients.jsx

--- app/components/clients.jsx
+++ app/components/clients.jsx
@@ -5,28 +5,16 @@ import React from 'react'
 const Clients = () => {
   return (
     <>
-      <div className='flex justify-center items-center text-4xl md:text-6xl font-semibold pb-5'>
-        <h1 className='text'><span className='text-5xl md:text-7xl text-white'>O</span>ur <span className='text-5xl md:text-7xl text-white'>C</span>lients</h1>
-      </div>
-      <div className='text-zinc-300 flex flex-col md:flex-row md:justify-around items-center gap-6 md:gap-0'>
-        <Link href='https://arx.vision/'>
-          <div className='text-3xl md:text-5xl p-4 flex gap-3 cursor-pointer transition-all duration-200 ease-in-out hover:border-b-2 hover:border-pink-500'>
-            <Image src='/ARX_vision.png' width={48} height={48} className='md:w-16 md:h-16' alt='image' />
-            <span className='text'>ARX Vision</span>
-          </div>
-        </Link>
-        <Link href='https://donadonations.com/'>
-          <div className='text-3xl md:text-5xl p-4 flex gap-3 cursor-pointer transition-all duration-200 ease-in-out hover:border-b-2 hover:border-pink-500'>
-            <Image src='/Dona.png' width={48} height={48} className='md:w-16 md:h-16' alt='image' />
-            <span className='text'>Dona Donations</span>
-          </div>
-        </Link>
-        <Link href='https://ourmedia.co.uk/'>
-          <div className='text-3xl md:text-5xl p-4 flex gap-3 cursor-pointer transition-all duration-200 ease-in-out hover:border-b-2 hover:border-blue-500'>
-            <Image src='/Our_media.png' width={48} height={48} className='md:w-16 md:h-16' alt='image' />
-            <span className='text'>Our Media</span>
-          </div>
-        </Link>
+      <div className='flex justify-center items-center w-full'>
+        <div className='text-white'>
+          <h1 className='text-xl text'>OUR AWESOME CLIENT</h1>
+          <h1 className='text-4xl font-semibold'>We Built Best<br/> Solution For Your<br/> Company</h1>
+        </div>
+        <div className=''>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </>
   )
