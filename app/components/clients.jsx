1daf5f0a329a3a252e9753c668b4353bf68082e1
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-27 19:16:27 +0530

    Update clients.jsx

--- app/components/clients.jsx
+++ app/components/clients.jsx
@@ -7,30 +7,32 @@ const Clients = () => {
     <>
       <div className='flex justify-center items-center w-full gap-5'>
         <div className='text-white'>
-          <h1 className='text-xl text'>OUR AWESOME CLIENT</h1>
-          <h1 className='text-4xl font-semibold'>We Built Best<br/> Solution For Your<br/> Company</h1>
+          <h1 className='text-3xl text pb-4'>OUR AWESOME CLIENT</h1>
+          <h1 className='text-6xl font-semibold'>We Built Best<br /> Solution For Your<br /> Company</h1>
         </div>
-        <div className=' flex justify-center items-center gap-4'>
+        <div className=' flex flex-col justify-center items-center gap-4'>
+          <div className='flex justify-center items-center gap-4'>
+            <div>
+              <Image
+                src='/ARX_vision.png'
+                height={64}
+                width={64}
+                alt='ARX VISION'
+                className=' border-2 border-white p-2 h-[20vh] w-[20vw]'
+              />
+            </div>
+            <div>
+              <Image
+                src='/Dona.png'
+                height={64}
+                width={64}
+                alt='DONA'
+                className=' border-2 border-white p-2 h-[20vh] w-[20vw]'
+              />
+            </div>
+          </div>
           <div>
             <Image
-              src='/ARX_vision.png'
-              height={64}
-              width={64}
-              alt='ARX VISION'
-              className=' border-2 border-white p-2 h-[20vh] w-[20vw]'
-            />
-          </div>
-          <div>
-          <Image
-              src='/Dona.png'
-              height={64}
-              width={64}
-              alt='DONA'
-              className=' border-2 border-white p-2 h-[20vh] w-[20vw]'
-            />
-          </div>
-          <div>
-          <Image
               src='/Our_media.png'
               height={64}
               width={64}
