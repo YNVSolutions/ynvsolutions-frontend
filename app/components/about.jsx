761a36b049fb42fb3a99922cc1caba3efe24e9be
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-28 17:23:55 +0530

    Update about.jsx

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <>
       <div 
         className="bg-cover bg-center h-screen w-full pt-5" 
-        style={{ backgroundImage: "url('/back.jpg')" }}
+        style={{ backgroundImage: "url('/back.png')" }}
       >
 
         
