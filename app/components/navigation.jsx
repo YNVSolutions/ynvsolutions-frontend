0e443aacc4fc2393ae7d960713ba1c6454848f59
Jitesh Singh <jiteshSingh4305@gmail.com>
2025-03-28 11:41:20 +0530

    changes to the contactUs button

--- app/components/navigation.jsx
+++ app/components/navigation.jsx
@@ -23,10 +23,14 @@ const Navigation = () => {
               {item.name}
             </a>
           ))}
-          <a href="#contactUs" className='p-2 text-xl font-medium capitalize bg-white hover:text-gray-800 text-black border-2 rounded-xl shadow-2xl border-gray-600'>
-            Contact Us
-          </a>
-        </div>
+          <a
+  href="#contactUs"
+  className="px-3 py-1.5 text-xl font-medium capitalize text-white bg-gray-800/90 backdrop-blur-sm border-2 border-gray-800 rounded-full hover:bg-gray-700/90 hover:backdrop-blur-md transition-all duration-200 ease-in-out"
+>
+  Contact Us
+</a>
+
+   </div>
 
         
         <button className="md:hidden text-3xl" onClick={() => setIsOpen(!isOpen)}>
