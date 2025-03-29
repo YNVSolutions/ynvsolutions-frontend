16450daa38ce5b9a400377c264d904bb4b3207dc
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-29 19:15:52 +0530

    Update about.jsx

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -18,7 +18,19 @@ const About = () => {
       </div>
       
       
-      
+      <div className="container mx-auto h-full flex items-center px-4 sm:px-6 lg:px-8">
+        <div className="text-white max-w-4xl relative z-10">
+          <h1 className='text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mb-4'>
+            <span className='text-6xl sm:text-7xl md:text-8xl lg:text-9xl'>Y</span>NV <span className='text-6xl sm:text-7xl md:text-8xl lg:text-9xl'>T</span>ech
+          </h1>
+          
+          <h2 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold border-b-2 border-white pb-2 mb-6 inline-block text'>
+            Driving Digital Transformation Through Innovation
+          </h2>
+          
+          
+        </div>
+      </div>
     </div>
   )
 }
