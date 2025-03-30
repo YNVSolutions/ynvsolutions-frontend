8bc50d7fb50597ffbf8393ee9835a98a925b898d
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-30 11:29:44 +0530

    Update serviceSection.jsx

--- app/components/serviceSection.jsx
+++ app/components/serviceSection.jsx
@@ -6,7 +6,7 @@ const ServiceSection = () => {
     {
       title: "App Development",
       description: "We craft innovative mobile applications for Android and iOS, focusing on efficiency and security. Our process includes design, development, deployment, testing, and maintenance. Explore our portfolio and benefit from our use of cutting-edge technologies.",
-      image:'/app.gif',
+      image: '/app.gif',
       offers: [
         "Cross-Platform Excellence",
         "End-to-End Development",
@@ -16,7 +16,7 @@ const ServiceSection = () => {
     {
       title: "MDM Solution",
       description: "Our MDM solution ensures complete privacy and security for your company devices. We offer device tracing, mobile management with data security, identity and access management, and BYOD authentication. Benefit from time savings, improved efficiency, and enhanced security.",
-      image:'/mdm.gif',
+      image: '/mdm.gif',
       offers: [
         "Device Tracking & Monitoring",
         "Secure Data Management",
@@ -26,7 +26,7 @@ const ServiceSection = () => {
     {
       title: "AI Solution",
       description: "Our AI solution enhances website/app performance through automated decision-making and machine learning. We analyze data to provide effective insights, mimicking human intelligence for chatbots, recommendations, and fraud detection.",
-      image:'/ai.gif',
+      image: '/ai.gif',
       offers: [
         "Automated Decisioning",
         "Machine Learning Core",
@@ -47,19 +47,21 @@ const ServiceSection = () => {
   return (
     <>
       <div className='flex justify-around items-center gap-2'>
-      {data.map((element, idx) => (
-        <div key={idx} className='bg-gradient-to-t from-gray-900/80 p-2 rounded-3xl'>
-          <img src={element.image} className='h-[25vh] w-[25vw] object-contain' alt=''/>
-          <h2 className="text-2xl font-bold text-white mb-3 bg-gradient-to-r bg-clip-text group-hover:drop-shadow-md transition-all duration-300 px-2">{element.title}</h2>
-          <p className="text-gray-200 text-sm leading-relaxed mb-6 font-light flex-grow px-2">{element.description}</p>
-          <ul>
-            <h1 className='px-3'>We Offer</h1>
-            {element.offers.map((offer, offerIdx) => (
-              <li key={offerIdx} className="text-gray-300 text-sm mb-1 px-2">- {offer}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+        {data.map((element, idx) => (
+          <div key={idx} className='bg-gradient-to-t from-gray-900/80 p-2 rounded-3xl'>
+            <img src={element.image} className='h-[25vh] w-[25vw] object-contain' alt='' />
+            <h2 className="text-2xl font-bold text-white mb-3 bg-gradient-to-r bg-clip-text group-hover:drop-shadow-md transition-all duration-300 px-2">{element.title}</h2>
+            <p className="text-gray-200 text-sm leading-relaxed mb-6 font-light flex-grow px-2">
+              {element.description}
+            </p>
+            <ul>
+              <h1 className='px-3'>We Offer</h1>
+              {element.offers.map((offer, offerIdx) => (
+                <li key={offerIdx} className="text-gray-300 text-sm mb-1 px-2">- {offer}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </>
   )
